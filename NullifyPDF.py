604776770d3024d9c9e2b850724b427e9aa66c27
0verwrite <31691645+overwrite00@users.noreply.github.com>
2026-09-18 10:17:33 +0200

    chore: bump version to 2.2.0-beta.2 for beta testing cycle

--- NullifyPDF.py
+++ NullifyPDF.py
@@ -60,7 +60,7 @@ from privacy_core import (
 )
 
 __version__ = "2.2.0"
-__version_prerelease__ = "beta.1"
+__version_prerelease__ = "beta.2"
 APP_VERSION = (
     f"{__version__}-{__version_prerelease__}"
     if __version_prerelease__
