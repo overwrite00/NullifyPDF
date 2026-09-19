595d54d69d77279bc4266d20ebd5322fe5b41e87
0verwrite <31691645+overwrite00@users.noreply.github.com>
2026-09-19 10:01:05 +0200

    chore: bump version to 2.3.0-beta.2

--- NullifyPDF.py
+++ NullifyPDF.py
@@ -82,7 +82,7 @@ PDF_ANNOT_REDACT: int = fitz.PDF_ANNOT_REDACT  # type: ignore[attr-defined]
 PDF_REDACT_IMAGE_PIXELS: int = fitz.PDF_REDACT_IMAGE_PIXELS  # type: ignore[attr-defined]
 
 __version__ = "2.3.0"
-__version_prerelease__ = "beta.1"
+__version_prerelease__ = "beta.2"
 APP_VERSION = (
     f"{__version__}-{__version_prerelease__}"
     if __version_prerelease__
